7659d32deee40b46113f20d4cdb6267b334972b3
NiAlKo <35404879+NiAlKo@users.noreply.github.com>
2022-02-08 18:30:24 +0300

    Adds fixes related to indentation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 // BTN CATALOG TOGGLE
-let btnToggleCatalog = document.querySelector(".catalog-toggler-btn");
-let catalogNav = document.querySelector(".catalog-toggler-nav");
+const btnToggleCatalog = document.querySelector(".catalog-toggler-btn");
+const catalogNav = document.querySelector(".catalog-toggler-nav");
 
 // BTN CATALOG ACTION
 btnToggleCatalog.addEventListener("click", clickBtnCatalog);
@@ -10,50 +10,96 @@ function clickBtnCatalog(evt){
 }
 
 // SLIDER
-let btnSliderPrev = document.querySelector(".slider-btn-prev");
-let btnSliderNext = document.querySelector(".slider-btn-next");
-let slides = document.querySelectorAll(".slide");
-let sliderCounter = document.querySelector(".slider-counter");
-let btnSliderPagination = document.querySelectorAll(".slider-pagination-btn");
+const btnSliderPrev = document.querySelector(".slider-btn-prev");
+const btnSliderNext = document.querySelector(".slider-btn-next");
+const slides = document.querySelectorAll(".slide");
+const sliderCounter = document.querySelector(".slider-counter");
+const btnSliderPagination = document.querySelectorAll(".slider-pagination-btn");
 
-/* Install the default slide and btns index */
-let slideIndex = 0;
-showSlides(slideIndex);
+/* Index of the current element */
+function getActiveSlideIndex() {
+  for(let i=0;i<slides.length;i++){
+if (slides[i].classList.contains("slide--show")){
+ return i;
+}
+}}
 
+/*Show the following slide*/
 btnSliderNext.addEventListener("click", nextSlide);
-/* Increase the index on 1 - show the following slide*/
 function nextSlide() {
-    showSlides(slideIndex += 1);
-
+  setSlide(getActiveSlideIndex() + 1);
 }
 
+/*Show the previous slide*/
 btnSliderPrev.addEventListener("click", previousSlide);
-/*Reduces index on 1 - show the previous slide*/
 function previousSlide() {
-    showSlides(slideIndex -= 1);
-
+  setSlide(getActiveSlideIndex() - 1);
 }
+
 /* Swipe btns*/
 btnSliderPagination.forEach((element, index) => {
   element.addEventListener("click", () => {
-     showSlides(index);
+    setSlide(index);
     });
  })
 
-/* Swipe slides function*/
-function showSlides(n) {
+/*Choosing the next slide*/
+function setSlide(n) {
   slideIndex = (n + slides.length) % slides.length;
-  sliderCounter.innerText = "0" + (slideIndex +1);
+  setActiveSlideElement();
+  updateSlideText();
+  setActiveSlideDot();
+}
 
-  for (let slide of slides) {
-      slide.classList.remove("slide--show");
-  }
-  slides[slideIndex].classList.add("slide--show");
+/*Changes the text of the slide сounter*/
+function updateSlideText(){
+sliderCounter.innerText = "0" + (slideIndex +1);
+}
 
-  for (let btn of btnSliderPagination) {
+/*Chooses the active dot of the slider*/
+function setActiveSlideDot(){
+ for (let btn of btnSliderPagination) {
     btn.classList.remove("slider-pagination-btn--current");
   }
   btnSliderPagination[slideIndex].classList.add("slider-pagination-btn--current");
 }
+/*Chooses an active slide*/
+function setActiveSlideElement(){
+  for (let slide of slides) {
+      slide.classList.remove("slide--show");
+  }
+  slides[slideIndex].classList.add("slide--show");
+}
 
 
+// SERVICES
+const serviceItem = document.querySelectorAll(".services-nav-item");
+const serviceBtn = document.querySelectorAll(".services-nav-btn");
+const serviceDesc = document.querySelectorAll(".services-desc-item");
+
+/*Choosing the next item*/
+function showService(n) {
+  serviceIndex = (n + serviceItem.length) % serviceItem.length;
+  setActiveServiceItem();
+setActiveServiceBtn();
+}
+/*Chooses an active item*/
+function setActiveServiceItem(){
+  for (let desc of serviceDesc) {
+    desc.classList.remove("service--show");
+}
+serviceDesc[serviceIndex].classList.add("service--show");
+}
+/*Chooses an active btn*/
+function setActiveServiceBtn(){
+for (let service of serviceItem) {
+    service.classList.remove("services-nav-item--active");
+}
+serviceItem[serviceIndex].classList.add("services-nav-item--active");
+}
+/* Swipe btns*/
+serviceBtn.forEach((element, index) => {
+  element.addEventListener("click", () => {
+    showService(index);
+    });
+ })
